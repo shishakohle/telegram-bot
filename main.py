771a96ea8c20ae4617785ee88d8d27190b8b5408
Ingo Weigel <me19m022@technikum-wien.at>
2022-07-05 04:12:47 +0200

    added /weather command to usage example

--- main.py
+++ main.py
@@ -2,17 +2,71 @@
 
 from TelegramBot import TelegramBot
 import GuteFrageNet
+import requests
+import json
+import datetime
+import dateutil.parser
 
 
 def handle_message(message: dict):
     # check if message has a key 'text' (messages that indicates bot has been added to new channel does not have a text)
     if 'text' in message:
-        # extract first word in the message
-        first_word = message['text'].split(' ', 1)[0]
+        # seperate message/command into single ordered words
+        words = message['text'].split(' ')
+        command = words[0]
+        parameters = words[1:]
         # handle the /frage command
-        if first_word == "/frage" or first_word == "/frage@MaHueWG_bot":
+        if command == "/frage" or command == "/frage@MaHueWG_bot":
             question = GuteFrageNet.latest_question()
-            msg = "The latest question on gutefrage.net is: " + question
+            msg = "The latest question on gutefrage.net is:\n\n" + question
+            mahue_bot.telegram_query("sendMessage", {'chat_id': message['chat']['id'], 'text': msg})
+        # handle the /regen command
+        elif command == "/weather" or command == "/weather@MaHueWG_bot":
+            locations = {
+                'Vienna':  {'latitude': "48.2085", 'longitude': "16.3725"},
+                'Ottenöd': {'latitude': "48.5806", 'longitude': "13.2418"}
+                }
+            if not parameters:  # no parameters provided
+                mahue_bot.telegram_query("sendMessage", {'chat_id': message['chat']['id'], 'text': "Please provide a location along this command, like:\n`/weather <location>`", 'parse_mode': "Markdown"})
+                return
+            elif parameters[0] not in locations:
+                msg  = "Sorry, I've no weather forecast for " + parameters[0] + ".\n\n"
+                msg += "I could provide you with forecasts for:"
+                for location in locations.keys():
+                    msg += "\n- `" + location + "`"
+                mahue_bot.telegram_query("sendMessage", {'chat_id': message['chat']['id'], 'text': msg, 'parse_mode': "Markdown"})
+                return
+            else:
+                location = parameters[0]
+                coordinates = locations[location]
+            url = "https://api.open-meteo.com/v1/forecast?latitude="\
+                  + coordinates['latitude'] + "&longitude=" + coordinates['longitude'] \
+                  + "&hourly=temperature_2m,precipitation&current_weather=true&timezone=Europe%2FBerlin"
+            try:
+                request = requests.get(url)
+            except requests.exceptions.RequestException as e:
+                mahue_bot.telegram_query("sendMessage", {'chat_id': message['chat']['id'], 'text': "Sorry, my request for weather data failed. Try again later."})
+            else:
+                request.close()
+                response = json.loads(request.text)
+                msg  = "Currently " + str(response['current_weather']['temperature']) + " °C in " + location + ".\n\n"
+                msg += "Temperature and precipitation expected:"
+                timestamps = response['hourly']['time']
+                temperatures = response['hourly']['temperature_2m']
+                precipitations = response['hourly']['precipitation']
+                counter = 1
+                for i in range(0, len(timestamps)):
+                    date = dateutil.parser.parse(timestamps[i])
+                    current = datetime.datetime.now()
+                    if date > current and counter <= 12:
+                        msg += "\n0" if date.hour < 10 else "\n"
+                        msg += str(date.hour) + (" am: " if date.hour < 12 else " pm: ") + str(temperatures[i]) + " °C, " + str(precipitations[i]) + " mm"
+                        counter += 1
+                msg += "\n\n[Weather data by Open-Meteo.com](https://open-meteo.com/)\nunder [CC BY-NC 4.0](https://creativecommons.org/licenses/by-nc/4.0/)."
+                mahue_bot.telegram_query("sendMessage", {'chat_id': message['chat']['id'], 'text': msg, 'parse_mode': "Markdown"})
+        # unknown command
+        else:
+            msg = "Sorry, I don't know this command: " + command + "\n\nI know:\n/frage\n/weather"
             mahue_bot.telegram_query("sendMessage", {'chat_id': message['chat']['id'], 'text': msg})
 
 
